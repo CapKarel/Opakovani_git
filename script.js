84413114125182059200322add8918190240f2f9
CapKarel <cap.karel@zak.spspb.cz>
2026-06-16 12:47:03 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('actionButton').addEventListener('click', function() {
+    document.getElementById('message').innerText = 'JavaScript funguje!';
+    document.body.style.backgroundColor = '#e0f7fa';
+});
